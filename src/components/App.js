dfeccac6a1457a556fcf9990ceec9f777e4fa36d
Evgeniya Marchenkova <evgeniova@gmail.com>
2017-06-06 23:46:58 +0300

    rs-calendar: bug wuth routing fixed

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ const moment = extendMoment(Moment);
 import Calendar from './Calendar';
 import CalendarMonth from './Model/CalendarMonth';
 import CalendarWeek from './Model/CalendarWeek'
+import NavLink from './NavLink'
 
 
 
@@ -41,7 +42,14 @@ export default class  extends React.Component {
             calendarWeek
         } = this.props
         return (
-            <Calendar {...this.props}  /* switchToWeek = {this.switchToWeek} switchToMonth = {this.switchToMonth} *//>
+            <div>
+                <h1>RS-calendar</h1>
+                    <ul role="nav">
+                    <li><NavLink onClick={this.switchToWeek} to="/weeks">Weeks</NavLink></li>
+                    <li><NavLink onClick={this.switchToMonth} to="/month">Month</NavLink></li>
+                    </ul>
+                {this.props.children}
+            </div>
         )
     }
 }
